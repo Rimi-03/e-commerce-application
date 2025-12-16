c8b66ef387cf75ca3fb56d129d459a7e2d1a259c
Naima Rahman <naimarahman006@gmail.com>
2025-12-16 18:50:34 +0600

    Added Home Screen header with avatar, notification, cart, theme button, and custom search bar

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:ecommerce_app/controllers/theme_controller.dart';
+import 'package:ecommerce_app/view/widgets/custom_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
@@ -7,9 +10,73 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        body: SafeArea(
+          child: Column (
+            children: [
+              // header section
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('assets/images/avatar.jpg'),
+                    ),
+                    SizedBox(width: 12),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Hello Alex',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          'Good Morning',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                    Spacer(),
+                    //notification icon
+                    IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                            Icons.notifications_outlined
+                        ),
+                    ),
+                    //cart button
+                    IconButton(
+                      onPressed: (){},
+                      icon: Icon(
+                          Icons.shopping_bag_outlined
+                      ),
+                    ),
+                    //theme button
+                    GetBuilder<ThemeController>(
+                        builder: (controller) =>  IconButton(
+                            onPressed: () => controller.toggleTheme(),
+                            icon: Icon(
+                              controller.isDarkMode ? Icons.light_mode :
+                              Icons.dark_mode,
+                            ),
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+
+              //search bar
+              const CustomSearchBar(),
+            ],
+          ),
+        ),
     );
   }
 }
