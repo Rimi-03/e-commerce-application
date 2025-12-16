8acc5b96230ec6a97d1d37879ca47a536b8c2088
Rimi-03 <chowdhuryrimi59@gmail.com>
2025-12-16 15:36:41 +0600

    Make changes in Product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,7 +1,58 @@
-const Product(
-isFavorite: true,
-),
+class Product {
+  final String name;
+  final String category;
+  final double price;
+  final double? oldPrice;
+  final String imageUrl;
+  final bool isFavorite;
+  final String description;
 
-const Product(
-isFavorite: true,
-),
+  const Product({
+    required this.name,
+    required this.category,
+    required this.price,
+    this.oldPrice,
+
+    required this.imageUrl,
+    required this.description,
+
+    this.isFavorite = false,
+  });
+}
+
+final List<Product> products = [
+  const Product(
+    name: 'Shoes',
+    category: 'Footwear',
+    price: 69.00,
+    oldPrice: 189.00,
+    imageUrl: 'assets/images/shoe.jpg',
+    description: 'This is a description of product 1',
+  ),
+  const Product(
+    name: 'Laptop',
+    category: 'Electronics',
+    price: 69.00,
+    oldPrice: 189.00,
+    imageUrl: 'assets/images/laptop.jpg',
+    description: 'This is a description of product 2',
+  ),
+  const Product(
+    name: 'Jordan Shoes',
+    category: 'Footwear',
+    price: 69.00,
+    oldPrice: 189.00,
+    imageUrl: 'assets/images/shoe2.jpg',
+    description: 'This is a description of product 3',
+    isFavorite: true,
+  ),
+  const Product(
+    name: 'Puma',
+    category: 'Footwear',
+    price: 69.00,
+    oldPrice: 189.00,
+    imageUrl: 'assets/images/shoe2.jpg',
+    description: 'This is a description of product 4',
+    isFavorite: true,
+  ),
+];
