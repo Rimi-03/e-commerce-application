cdaa4e78700f973ee51150940d38a0fa1e3d8c0a
Rimi-03 <chowdhuryrimi59@gmail.com>
2025-12-16 14:10:17 +0600

    Added wishlist screen

---
+++ lib/view/wishlist_screen.dart
@@ -0,0 +1,79 @@
+import 'package:ecommerce_app/utils/app_textstyles.dart';
+import 'package:flutter/material.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      appBar: AppBar(
+        title: Text(
+          'My Wishlist',
+          style: AppTextStyle.withColor(
+            AppTextStyle.h3,
+            isDark ? Colors.white : Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: isDark ? Colors.white : Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          //summary section
+          SliverToBoxAdapter(child: _buildSummarySection(context)),
+
+          //wishlist items
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSummarySection(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final favoriteProducts = products.where((p) => p.isFavorite).length;
+
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: isDark ? Colors.grey[850] : Colors.grey[100],
+        borderRadius: const BorderRadius.vertical(bottom: Radius.circular(16)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '$favoriteProducts Items',
+                style: AppTextStyle.withColor(
+                  AppTextStyle.h2,
+                  Theme.of(context).textTheme.bodyLarge!.color!,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                'in your wishlist',
+                style: AppTextStyle.withColor(
+                  AppTextStyle.bodyMedium,
+                  isDark ? Colors.grey[400]! : Colors.grey[600]!,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
