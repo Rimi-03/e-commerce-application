3568619a6fd3ccb29bf5eb2c375ff2f599536d31
Naima Rahman <naimarahman006@gmail.com>
2025-12-18 22:56:14 +0600

    Set up edit profile screen files

---
+++ lib/view/help center/views/widgets/popular_questions_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PopularQuestionsSection extends StatelessWidget {
+  const PopularQuestionsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
