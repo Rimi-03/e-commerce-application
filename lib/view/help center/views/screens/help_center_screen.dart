1e9929e7dd71f58487291b070905e672ea8d6084
Naima Rahman <naimarahman006@gmail.com>
2025-12-19 01:27:09 +0600

    Create popular questions section

--- lib/view/help center/views/screens/help_center_screen.dart
+++ lib/view/help center/views/screens/help_center_screen.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../../utils/app_textstyles.dart';
+import '../widgets/popular_questions_section.dart';
 
 class HelpCenterScreen extends StatelessWidget {
   const HelpCenterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton (
+          onPressed: ()=> Get.back(),
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        title: Text(
+          'Help Center',
+          style: AppTextStyle.withColor(
+            AppTextStyle.h3,
+            isDark ? Colors.white: Colors.black,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildSearchBar(context, isDark),
+            const SizedBox(height: 24),
+            const PopularQuestionsSection(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSearchBar(BuildContext context, bool isDark){
+    return Container(
+      margin: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Theme.of(context).cardColor,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: isDark?
+            Colors.black.withOpacity(0.2)
+                : Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search for help',
+          hintStyle: AppTextStyle.withColor(
+            AppTextStyle.bodyMedium,
+            isDark ? Colors.grey[400]! : Colors.grey[600]!,
+          ),
+          prefixIcon: Icon(
+            Icons.search,
+            color: isDark ? Colors.grey[400] : Colors.grey[600],
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none,
+          ),
+          filled: true,
+          fillColor: Theme.of(context).cardColor,
+        ),
+      ),
+    );
   }
 }
