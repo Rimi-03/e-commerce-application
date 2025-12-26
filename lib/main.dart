ef796a0d502c60d815eb03b2d09406ef96c92564
Nahida-Chowdhury <chowdhurynahida06@gmail.com>
2025-12-26 17:30:52 +0600

    Register product controller and initialize Firestore setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:ecommerce_app/controllers/auth_controller.dart';
 import 'package:ecommerce_app/controllers/navigation_controller.dart';
+import 'package:ecommerce_app/controllers/product_controller.dart';
 import 'package:ecommerce_app/controllers/theme_controller.dart';
 import 'package:ecommerce_app/firebase_options.dart';
 import 'package:ecommerce_app/utils/app_themes.dart';
+import 'package:ecommerce_app/utils/firestore_data_seeder.dart';
 import 'package:ecommerce_app/view/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +19,11 @@ void main() async{
   await GetStorage.init();
   Get.put(ThemeController());
   Get.put(AuthController());
+  Get.put(ProductController());
   Get.put(NavigationController());
+
+  //the line below is used to seed data sample to firestore(for testing only)
+  await FirestoreDataSeeder.seedAllData();
   runApp(const MyApp());
 }
 
